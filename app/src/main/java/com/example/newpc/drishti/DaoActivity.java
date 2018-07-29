be0d9e02d61a9fca72e31413d1230e6a1494dd39
anand singh <anand.sk0901@gmail.com>
2018-07-30 00:50:18 +0530

    Commit Drishti

---
+++ app/src/main/java/com/example/newpc/drishti/DaoActivity.java
@@ -0,0 +1,14 @@
+package com.example.newpc.drishti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.newpc.qrcode.R;
+
+public class DaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dao);
+    }
+}
